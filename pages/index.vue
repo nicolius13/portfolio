cef336f84b37149d36f98df7276bb79489d3eac3
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-07-10 16:17:41 +0700

    tab navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    <Header />
-    <Intro />
-    <About />
-    <Work />
+    <b-container>
+      <b-tabs content-class="mt-3" align="center">
+        <b-tab active>
+          <template v-slot:title>
+            <font-awesome-icon :icon="['fas', 'home']" />
+          </template>
+          <Intro />
+        </b-tab>
+        <b-tab>
+          <template v-slot:title>
+            <font-awesome-icon :icon="['fas', 'folder']" />
+          </template>
+          <Work />
+        </b-tab>
+        <b-tab>
+          <template v-slot:title>
+            <font-awesome-icon :icon="['fas', 'address-card']" />
+          </template>
+          <About />
+        </b-tab>
+        <b-tab>
+          <template v-slot:title>
+            <font-awesome-icon :icon="['fas', 'envelope']" />
+          </template>
+          Contact me !
+        </b-tab>
+      </b-tabs>
+    </b-container>
+
     <Footer />
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header';
 import Intro from '@/components/Intro';
 import About from '@/components/About';
 import Work from '@/components/Work';
@@ -17,7 +41,6 @@ import Footer from '@/components/Footer';
 
 export default {
   components: {
-    Header,
     Intro,
     About,
     Work,
@@ -26,4 +49,57 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.nav-tabs {
+  border: none;
+}
+
+.tab-content {
+  padding: 2em;
+  background: $clr-light;
+}
+
+.nav-tabs .nav-link {
+  position: relative;
+  display: inline-block;
+  color: $clr-light;
+  opacity: 0.4;
+  border: none;
+  @include font-size(2.8rem);
+  margin: 0 0.25em 0 0.25em;
+  transition: opacity 0.25s ease-in-out;
+}
+.nav-tabs .nav-link:hover {
+  opacity: 1;
+}
+
+.nav-tabs .nav-link.active {
+  color: $clr-light;
+  background: none;
+  opacity: 1;
+}
+
+.nav-tabs .nav-link::after {
+  content: '';
+  position: absolute;
+  display: block;
+  bottom: -1rem;
+  margin: auto;
+  border-bottom: 0rem solid $clr-light;
+  border-left: solid transparent;
+  @include rfs(1.5rem, border-left-width);
+  border-right: solid transparent;
+  @include rfs(1.5rem, border-right-width);
+  transition: border-bottom-width 0.25s ease-in-out;
+}
+
+@media screen and (max-width: 700px) {
+  .nav-tabs .nav-link::after {
+    left: 0.75rem;
+  }
+}
+
+.nav-tabs .nav-link.active::after {
+  @include rfs(1.5rem, border-bottom-width);
+}
+</style>
