fdefda945adafe49762ea60594209ab0abf180cd
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-07-21 15:44:42 +0700

    create projectCard component and style cards

---
+++ components/UI/ProjectCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-col class="d-flex align-items-stretch" md="6" lg="4">
+    <b-card
+      :title="project.title"
+      :img-src="project.imgSrc"
+      :img-alt="project.imgAlt"
+      class="work_cards"
+      footer-class="work_footer"
+      img-top
+    >
+      <b-card-text>
+        {{ project.description }}
+      </b-card-text>
+
+      <template v-slot:footer>
+        <a v-if="project.liveLink" :href="project.liveLink" target="blank">
+          <font-awesome-icon :icon="['fas', 'eye']" />
+          See Live
+        </a>
+        <a v-if="project.codeLink" :href="project.codeLink" target="blank">
+          <font-awesome-icon :icon="['fab', 'github']" />
+          Code
+        </a>
+      </template>
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.work_cards {
+  margin: 1em 0;
+}
+
+.work_footer {
+  background-color: $clr-light;
+  border-top: none;
+
+  a {
+    padding-right: 1em;
+    color: $clr-accent2;
+    transition: all 0.25s ease-in-out;
+  }
+  a:hover {
+    color: $clr-accent;
+  }
+}
+</style>
--- components/Work.vue
+++ components/Work.vue
@@ -3,245 +3,107 @@
     <section id="work" class="my-work">
       <h2 class="section_title">My work</h2>
       <b-row>
-        <!-- Projet Resto Finder -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Resto Finder"
-            img-src="~/assets/img/work/resto_finder.jpg"
-            img-alt="Resto Finder map"
-            img-top
-          >
-            <b-card-text>
-              Project 7 created for the Front-End developer path of
-              OpenClassroom: Launch your own restaurant review site.
-            </b-card-text>
-
-            <template v-slot:footer>
-              <a
-                href="https://nicolius13.github.io/resto_rating"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fas', 'eye']" />
-                See Live
-              </a>
-              <a
-                href="https://github.com/nicolius13/resto_rating"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fab', 'github']" />
-                Code
-              </a>
-            </template>
-          </b-card>
-        </b-col>
-
-        <!-- Projet Bambous.be -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Bambous.be"
-            img-src="~/assets/img/work/bambous.jpg"
-            img-alt="Resto Finder map"
-            img-top
-          >
-            <b-card-text>
-              Modernisation et passage au CMS Wordpress du site bambous.be
-            </b-card-text>
-            <template v-slot:footer>
-              <a href="https://bambous.be/" target="blank">
-                <font-awesome-icon :icon="['fas', 'eye']" />
-                See Live
-              </a>
-            </template>
-          </b-card>
-        </b-col>
-
-        <!-- Projet Dungeon -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Dungeon"
-            img-src="~/assets/img/work/Dungeon.jpg"
-            img-alt="Dungeon game"
-            img-top
-          >
-            <b-card-text>
-              Project 6 created for the Front-End developer path of
-              OpenClassroom: Build a turn-based board game in JavaScript.
-            </b-card-text>
-
-            <template v-slot:footer>
-              <a
-                href="https://nicolius13.github.io/Dungeon_code"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fas', 'eye']" />
-                See Live
-              </a>
-              <a
-                href="https://github.com/nicolius13/Dungeon_code"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fab', 'github']" />
-                Code
-              </a>
-            </template>
-          </b-card>
-        </b-col>
-
-        <!-- Projet Testing Todos -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Enhance an existing project"
-            img-src="~/assets/img/work/testing_todos.png"
-            img-alt="Todo App"
-            img-top
-          >
-            <b-card-text>
-              Project 8 created for the Front-End developer path of
-              OpenClassroom: Enhance an existing project.
-            </b-card-text>
-
-            <template v-slot:footer>
-              <a
-                href="https://nicolius13.github.io/testing-todo"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fas', 'eye']" />
-                See Live
-              </a>
-              <a
-                href="https://github.com/nicolius13/testing-todo"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fab', 'github']" />
-                Code
-              </a>
-            </template>
-          </b-card>
-        </b-col>
-
-        <!-- Projet Quotes Generator  -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Qutoes Generator"
-            img-src="~/assets/img/work/quotes_generator.png"
-            img-alt="Quotes generator"
-            img-top
-          >
-            <b-card-text>
-              Project 5 created for the Front-End developer path of
-              OpenClassroom: Create a quotes generator.
-            </b-card-text>
-
-            <template v-slot:footer>
-              <a
-                href="https://nicolius13.github.io/Quotes-generator/"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fas', 'eye']" />
-                See Live
-              </a>
-              <a
-                href="https://github.com/nicolius13/Quotes-generator"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fab', 'github']" />
-                Code
-              </a>
-            </template>
-          </b-card>
-        </b-col>
-
-        <!-- Projet Film Festival  -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Analyze a client's needs"
-            img-src="~/assets/img/work/film_festival.jpg"
-            img-alt="film festival website"
-            img-top
-          >
-            <b-card-text>
-              Project 3 created for the Front-End developer path of
-              OpenClassroom: Analyze a client's needs for their film festival.
-            </b-card-text>
-
-            <template v-slot:footer>
-              <a
-                href="https://nicolius13.github.io/simple-film-festival-website"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fas', 'eye']" />
-                See Live
-              </a>
-              <a
-                href="https://github.com/nicolius13/simple-film-festival-website"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fab', 'github']" />
-                Code
-              </a>
-            </template>
-          </b-card>
-        </b-col>
-
-        <!-- Projet Express Food  -->
-        <b-col class="d-flex align-items-stretch" md="6" lg="4">
-          <b-card
-            class="work_cards"
-            footer-class="work_footer"
-            title="Map out a technical solution"
-            img-src="~/assets/img/work/express_food.png"
-            img-alt="express food doc"
-            img-top
-          >
-            <b-card-text>
-              Project 4 - created for the Front-End developer path of
-              OpenClassroom: Map out a technical solution for the food delivery
-              site ExpressFood.
-            </b-card-text>
-
-            <template v-slot:footer>
-              <a
-                href="https://github.com/nicolius13/express_food"
-                target="blank"
-              >
-                <font-awesome-icon :icon="['fab', 'github']" />
-                Doc
-              </a>
-            </template>
-          </b-card>
-        </b-col>
+        <ProjectCard
+          v-for="project in projects"
+          :key="project.title"
+          :project="project"
+        />
       </b-row>
     </section>
   </b-container>
 </template>
 
 <script>
-export default {};
+import ProjectCard from './UI/ProjectCard';
+export default {
+  components: {
+    ProjectCard,
+  },
+  data() {
+    return {
+      projects: [
+        // Projet Resto Finder
+        {
+          title: 'Resto Finder',
+          imgSrc: require('~/assets/img/work/resto_finder.jpg'),
+          imgAlt: 'Resto Finder map',
+          description:
+            'Project 7 created for the Front-End developer path of OpenClassroom: Launch your own restaurant review site.',
+          liveLink: 'https://nicolius13.github.io/resto_rating',
+          codeLink: 'https://github.com/nicolius13/resto_rating',
+        },
+        //  Projet Bambous.be
+        {
+          title: 'Bambous.be',
+          imgSrc: require('~/assets/img/work/bambous.jpg'),
+          imgAlt: 'bambous.be website',
+          description:
+            'Redesign and switching of bambous.be web site to WordPress',
+          liveLink: 'https://bambous.be/',
+        },
+        // Projet Dungeon
+        {
+          title: 'Dungeon',
+          imgSrc: require('~/assets/img/work/Dungeon.jpg'),
+          imgAlt: 'Dungeon game',
+          description:
+            'Project 6 created for the Front-End developer path of OpenClassroom: Build a turn-based board game in JavaScript.',
+          liveLink: 'https://nicolius13.github.io/Dungeon_code',
+          codeLink: 'https://github.com/nicolius13/Dungeon_code',
+        },
+        // Projet Testing Todos
+        {
+          title: 'Enhance an existing project',
+          imgSrc: require('~/assets/img/work/testing_todos.png'),
+          imgAlt: 'Todo App',
+          description:
+            'Project 8 created for the Front-End developer path of OpenClassroom: Enhance an existing project.',
+          liveLink: 'https://nicolius13.github.io/testing-todo',
+          codeLink: 'https://github.com/nicolius13/testing-todo',
+        },
+        // Projet Quotes Generator
+        {
+          title: 'Quotes Generator',
+          imgSrc: require('~/assets/img/work/quotes_generator.png'),
+          imgAlt: 'Quotes generator',
+          description:
+            'Project 5 created for the Front-End developer path of OpenClassroom: Create a quotes generator.',
+          liveLink: 'https://nicolius13.github.io/Quotes-generator',
+          codeLink: 'https://github.com/nicolius13/Quotes-generator',
+        },
+        // Projet Film Festival
+        {
+          title: "Analyze a client's needs",
+          imgSrc: require('~/assets/img/work/film_festival.jpg'),
+          imgAlt: 'film festival website',
+          description:
+            "Project 3 created for the Front-End developer path of OpenClassroom: Analyze a client's needs for their film festival.",
+          liveLink: 'https://nicolius13.github.io/simple-film-festival-website',
+          codeLink:
+            'https://github.com/nicolius13/simple-film-festival-website',
+        },
+        // Projet Express Food
+        {
+          title: 'Map out a technical solution',
+          imgSrc: require('~/assets/img/work/express_food.png'),
+          imgAlt: 'express food doc',
+          description:
+            'Project 4 created for the Front-End developer path of OpenClassroom: Map out a technical solution for the food delivery site ExpressFood.',
+
+          codeLink: 'https://github.com/nicolius13/express_food',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.work_cards {
-  margin: 1em 0;
-}
-
-.work_footer {
-  background-color: $clr-light;
-  border-top: none;
-
-  a {
-    padding-right: 1em;
-  }
+.section_title {
+  background: $clr-accent;
+  font-family: $ff-secondary;
+  @include font-size(1.75rem);
+  padding: 0.5em 1em;
+  margin-bottom: 1em;
 }
 </style>
