fd21cedf2ba8b81d267268453f4ec148e3fb1e88
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-07-28 10:57:10 +0700

    contact pg trad

--- lang/en-US.js
+++ lang/en-US.js
@@ -4,7 +4,20 @@ export default {
   come: 'Come and see',
   canDo: 'what I can do',
 
-  // Work page
+  // WORK page
   work: 'My work',
   make: 'I make fast and dynamic web applications',
+
+  // CONTACT page
+  contact: 'Contact me',
+  nameLabel: 'Your Name:',
+  namePH: 'Enter name',
+  mailLabel: 'Your Email Adress:',
+  mailPH: 'Enter email',
+  mailDis: "I'll never share your email with anyone else.",
+  msgLabel: 'your Message:',
+  msgPH: 'Enter something...',
+  submitBtn: 'Submit',
+  thx: 'Thank You!',
+  getBack: 'I will get back to you soon!',
 };
--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -7,4 +7,17 @@ export default {
   // Work page
   work: 'Mon travail',
   make: 'Je fais des web app rapides et dynamiques',
+
+  // CONTACT page
+  contact: 'Me contater',
+  nameLabel: 'Votre Nom:',
+  namePH: 'nom',
+  mailLabel: 'Votre Adresse Email:',
+  mailPH: 'email',
+  mailDis: "Je ne partagerai jamais votre email avec quelqu'un d'autre.",
+  msgLabel: 'Votre Message:',
+  msgPH: 'Tapez quelque chose...',
+  submitBtn: 'Envoyer',
+  thx: 'Merci!',
+  getBack: 'Je vous recontacterai au plus vite!',
 };
