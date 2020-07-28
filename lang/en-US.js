2675a6b9ab9cf3b1b12773769d2779e69cd1c914
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-07-28 11:14:01 +0700

    about pg trad

--- lang/en-US.js
+++ lang/en-US.js
@@ -8,6 +8,18 @@ export default {
   work: 'My work',
   make: 'I make fast and dynamic web applications',
 
+  // ABOUT page
+  who: 'Who I am',
+  belgian: 'Belgian Front-end developer based in Laos',
+  present1:
+    "Hello, I'm Nicolas Vastrade, I'm a Freelance Developer specialized in Vue.Js/Nuxt.js.",
+  present2:
+    "I used to work as a lab technologist in the pharmaceutical industry. When I moved to Senegal I took this opportunity to make a career change. I've always been passionate about IT and the web world and therefore decided to self-train in web development at first. I then followed the ‘Front-end Developer’ Certified Training on OpenClassroom. I'm now looking for new projects! ",
+  resume: 'My Resume',
+  check: 'Come check out what I’ve',
+  developed: 'already developed',
+  technos: 'Technologies that I use:',
+
   // CONTACT page
   contact: 'Contact me',
   nameLabel: 'Your Name:',
--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -8,6 +8,18 @@ export default {
   work: 'Mon travail',
   make: 'Je fais des web app rapides et dynamiques',
 
+  // ABOUT page
+  who: 'Qui je suis',
+  belgian: 'Développeur front-end belge basé au Laos',
+  present1:
+    'Hello, je suis Nicolas Vastrade, je suis un Développeur Freelance spécialisé en Vue.js/Nuxt.js',
+  present2:
+    "J'ai travaillé dans l'industrie pharmaceutique comme technicien de laboratoire, j'ai ensuite profité d'un déménagement au Sénégal pour rediriger ma carrière. j'ai toujours été passioné par l'univers du web et du coups j'ai décidé de m'autoformer en développement web dnas un premier temps. J'ai ensuite suivi la formation certifiée 'Développeur d'application - Frontend' d'OpenClassroom. Je suis maintenant à la recherche de nouveaux projets!",
+  resume: 'Mon CV',
+  check: "Venez voir ce que j'ai",
+  developed: 'déja développé',
+  technos: "Technologies que j'utilise:",
+
   // CONTACT page
   contact: 'Me contater',
   nameLabel: 'Votre Nom:',
