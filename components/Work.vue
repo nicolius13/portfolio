5befe07201f940399e2351e982248121e1e5e7ca
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-08-08 17:35:41 +0700

    fix typo

--- components/Work.vue
+++ components/Work.vue
@@ -35,7 +35,7 @@ export default {
           description_en:
             'Project 7 created for the Front-End developer path of OpenClassroom: Launch your own restaurant review site.',
           description_fr:
-            "7eme projet créé pour le parcour 'Dévelopeur d'application - frontend' d'OpenClassroom : lancez votre propre site d'avis de restaurant",
+            "7eme projet créé pour le parcours 'Dévelopeur d'application - frontend' d'OpenClassroom : lancez votre propre site d'avis de restaurant",
           technos: ['Nuxt', 'Bootstrap Vue', "Google API's"],
           liveLink: 'https://nicolius13.github.io/resto_rating',
           codeLink: 'https://github.com/nicolius13/resto_rating',
@@ -61,7 +61,7 @@ export default {
           description_en:
             'Project 6 created for the Front-End developer path of OpenClassroom: Build a turn-based board game in JavaScript.',
           description_fr:
-            "6eme projet créé pour le parcour 'Dévelopeur d'application - frontend' d'OpenClassroom : Créez un jeu de plateau tour par tour en JS",
+            "6eme projet créé pour le parcours 'Dévelopeur d'application - frontend' d'OpenClassroom : Créez un jeu de plateau tour par tour en JS",
           technos: ['JS', 'JQuery', 'Vue'],
           liveLink: 'https://nicolius13.github.io/Dungeon_code',
           codeLink: 'https://github.com/nicolius13/Dungeon_code',
@@ -75,7 +75,7 @@ export default {
           description_en:
             'Project 8 created for the Front-End developer path of OpenClassroom: Enhance an existing project.',
           description_fr:
-            "8eme projet créé pour le parcour 'Dévelopeur d'application - frontend' d'OpenClassroom : Reprenez et améliorez un projet existant",
+            "8eme projet créé pour le parcours 'Dévelopeur d'application - frontend' d'OpenClassroom : Reprenez et améliorez un projet existant",
           technos: ['JS', 'Jasmine'],
           liveLink: 'https://nicolius13.github.io/testing-todo',
           codeLink: 'https://github.com/nicolius13/testing-todo',
@@ -89,7 +89,7 @@ export default {
           description_en:
             'Project 5 created for the Front-End developer path of OpenClassroom: Create a quotes generator.',
           description_fr:
-            "5eme projet créé pour le parcour 'Dévelopeur d'application - frontend' d'OpenClassroom : Imaginez un générateur de citation",
+            "5eme projet créé pour le parcours 'Dévelopeur d'application - frontend' d'OpenClassroom : Imaginez un générateur de citation",
           technos: ['JS', 'CSS', 'Bootstrap'],
           liveLink: 'https://nicolius13.github.io/Quotes-generator',
           codeLink: 'https://github.com/nicolius13/Quotes-generator',
@@ -103,7 +103,7 @@ export default {
           description_en:
             "Project 3 created for the Front-End developer path of OpenClassroom: Analyze a client's needs for their film festival.",
           description_fr:
-            "3eme projet créé pour le parcour 'Dévelopeur d'application - frontend' d'OpenClassroom : Analysez les besoins de votre client pour son festival de films",
+            "3eme projet créé pour le parcours 'Dévelopeur d'application - frontend' d'OpenClassroom : Analysez les besoins de votre client pour son festival de films",
           technos: ['JS', 'CSS', 'Bootstrap'],
           liveLink: 'https://nicolius13.github.io/simple-film-festival-website',
           codeLink:
@@ -118,7 +118,7 @@ export default {
           description_en:
             'Project 4 created for the Front-End developer path of OpenClassroom: Map out a technical solution for the food delivery site ExpressFood.',
           description_fr:
-            "4eme projet créé pour le parcour 'Dévelopeur d'application - frontend' d'OpenClassroom : Concevez la solution technique d'une application de restauration en ligne ExpressFood",
+            "4eme projet créé pour le parcours 'Dévelopeur d'application - frontend' d'OpenClassroom : Concevez la solution technique d'une application de restauration en ligne ExpressFood",
 
           codeLink: 'https://github.com/nicolius13/express_food',
         },
